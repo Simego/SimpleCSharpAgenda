f5183879283057baa7545539067549c4e5dacb6a
Simego <contato@thesimego.com>
2013-11-28 04:02:02 -0200

    Added whole project

---
+++ ControleDeContatos/View/TelaAtividade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ControleDeContatos.View
+{
+    public partial class TelaAtividade : Form
+    {
+        public Atividade atividade { get; set; }
+
+        public TelaAtividade(Atividade atv)
+        {
+            InitializeComponent();
+            this.atividade = atv;
+            this.richTextDescricao.Text = atividade.descricao;
+            this.dtpickData.Value = atv.data;
+        }
+
+        private void btnCancela_Click(object sender, EventArgs e)
+        {
+            this.atividade.descricao = null;
+            this.Close();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.atividade.descricao = richTextDescricao.Text;
+            this.atividade.data = dtpickData.Value;
+            this.Close();
+        }
+    }
+}
